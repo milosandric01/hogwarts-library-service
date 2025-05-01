c83a9c663a6513f6cfd1874091b981a056ec3f58
Milos Andric <milos.andric.business@gmail.com>
2025-05-01 17:49:07 +0200

    Remove unused code

--- src/main/java/com/homework/hogwartslibrary/domain/BookType.java
+++ src/main/java/com/homework/hogwartslibrary/domain/BookType.java
@@ -1,15 +1,7 @@
 package com.homework.hogwartslibrary.domain;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum BookType {
-    NEW_RELEASE ("New Releases"),
-    REGULAR ("Regular"),
-    OLD_EDITION ("Old Editions");
-
-    private final String value;
+    NEW_RELEASE,
+    REGULAR,
+    OLD_EDITION;
 }
